920a8138370ec53f5356f2ae52fd5dee9333249d
e9mgund <mayur.gund@enine.school>
2024-05-07 17:56:04 +0530

    Updates

--- inventory_management/inventorymanager/urls.py
+++ inventory_management/inventorymanager/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path , include
+from rest_framework import routers
+from . import views
+from django.views import generic
 
+app_name = "tournament"
 
 urlpatterns = [
-    path()
+    path("", generic.TemplateView.as_view(template_name="inventorymanager/base.html")),
 ]
