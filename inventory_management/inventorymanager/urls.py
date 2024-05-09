cb17a9371e8443f2fe7e76eef75a9713e7bf3a33
e9mgund <mayur.gund@enine.school>
2024-05-09 18:58:28 +0530

    Complted some template work

--- inventory_management/inventorymanager/urls.py
+++ inventory_management/inventorymanager/urls.py
@@ -6,8 +6,9 @@ from django.views import generic
 app_name = "tournament"
 
 urlpatterns = [
-    path("",views.AssetView.as_view(),name="assettype"),
+    path("",views.AssetView.as_view()),
     path("login/",generic.TemplateView.as_view(template_name="inventorymanager/login.html")),
     path("register/",generic.TemplateView.as_view(template_name="inventorymanager/register.html"),name="register"),
-    path("settings/",generic.TemplateView.as_view(template_name="inventorymanager/settings.html"))
+    path("settings/",generic.TemplateView.as_view(template_name="inventorymanager/settings.html")),
+    path("assettype/",generic.TemplateView.as_view(template_name="inventorymanager/assettype.html"),name="assettype"),
 ]
